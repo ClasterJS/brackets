5f5128740ed88130beb619ccaa0318ddf989b6c1
Alex Chikun <Claster85@gmail.com>
2020-10-06 14:16:05 +0300

    feat: add function to get an array of open and closed brackets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
   //start task
+  if ((str < 1) || (str.length % 2 !== 0)) {
+      return false;
+  }
+  let openBrackets  = [];
+  let closeBrackets = [];
+
+  for (let i = 0; i < bracketsConfig.legth; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  };
+
+
+
+
 }
